1373cc3e2533737311866798e401e266a8706dd9
James Brunet <james@brunet.dev>
2024-01-30 13:30:47 -0500

    Slightly change prompt for name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-name = input("What is your name? ")
+name = input("What is yo name? ")
 print("Hello, {}".format(name))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
