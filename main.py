3b35536e07f9c288c77d971a432bc80737932567
James Brunet <james@brunet.dev>
2024-01-30 13:25:11 -0500

    Enhance .gitignore and add input to "Hello World"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-print("Hello world!")
+name = input("What is your name? ")
+print("Hello, {}".format(name))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
